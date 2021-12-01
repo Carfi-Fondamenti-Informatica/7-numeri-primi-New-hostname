5d2256a08c2ec457885a2e72f19403d558b4d8de
New-hostname <92860067+New-hostname@users.noreply.github.com>
2021-12-01 14:46:01 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,26 @@
+#include "lib.h"
 
+bool primo(int n, int c)
+{
+  if(n == 1)
+  {
+    return false;
+  }
+  if(n % c == 0)
+  {
+    return false;
+  }
+  else
+  {
+    c++
+    
+    if(c == n)
+    {
+      return true;
+    }
+    else
+    {
+      primo(n, c);
+    }
+  }
+}
