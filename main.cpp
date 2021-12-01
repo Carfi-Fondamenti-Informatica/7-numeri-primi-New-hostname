262794f45702edde6ccdd8c68f862b01c024a912
New-hostname <92860067+New-hostname@users.noreply.github.com>
2021-12-01 14:42:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  
+  int n = 0, c = 2;
+  
+  cin >> n;
+  
+  if(primo(n, c))
+  {
+    cout << "numero primo" << endl;
+  }
+  else
+  {
+    cout << "numero non primo" << endl;
+  }
+  
   return 0;
 }
